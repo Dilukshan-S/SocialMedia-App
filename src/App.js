145571f64e8460ceeceae111cb56985ac8cc707e
Dilukshan <dilukshan.s@cloud99x.com>
2024-03-18 14:00:01 +0530

    pages

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { Link, Route, Routes } from "react-router-dom";
+import About from "./About";
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import Missing from "./Missing";
+import Nav from "./Nav";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/About">About</Link></li>
+          <li><Link to="/NewPost">NewPost</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/newpost" element={<NewPost />}/>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+      {/* <Header />
+      <Nav />
+      <Home />
+      <NewPost />
+      <PostPage />
+      <About />
+      <Missing />
+      <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
