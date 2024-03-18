fa38bac3a762e3deac325670a7845e2d07eccfe5
Dilukshan <dilukshan.s@cloud99x.com>
2024-03-18 17:35:17 +0530

    PageNavigations

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Missing from "./Missing";
 import Nav from "./Nav";
 import NewPost from "./NewPost";
 import PostPage from "./PostPage";
+import Post from "./Post";
+import PostLayout from "./PostLayout";
 
 function App() {
   return (
@@ -15,14 +17,26 @@ function App() {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/About">About</Link></li>
-          <li><Link to="/NewPost">NewPost</Link></li>
+          <li><Link to="/PostPage">PostPage</Link></li>
         </ul>
       </nav>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />}/>
         <Route path="/newpost" element={<NewPost />}/>
-        <Route path="/" element={<Home />}/>
+        {/* the below lines(nested) can also be written like: 
+          <Route path="/PostPage" element={<PostPage />}/>
+          <Route path="/PostPage/:id" element={<Post />}/>
+          <Route path="/PostPage/NewPost" element={<NewPost />}/>
+         */}
+         <Route path="/PostPage" element={ <PostLayout />}>
+          <Route index element={<PostPage/>} />
+          <Route path=":id" element={<Post />}/>
+          <Route path="NewPost" element={<NewPost />}/>
+        </Route>
+
+
+        <Route path="*" element={<Missing/>}/>
       </Routes>
       {/* <Header />
       <Nav />
